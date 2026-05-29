1b61bfc9d58b7b326fbedfb0309a131c60d20ea3
admon fixer <cscbarranquilla@fixer.repair>
2026-05-29 16:06:43 -0500

    oler

--- src/pages/public/Login.tsx
+++ src/pages/public/Login.tsx
@@ -145,7 +145,7 @@ export default function Login() {
               <>
                 ¿No tienes cuenta?
                 <button onClick={() => setMode("register")}>
-                  Crea tu usuario
+                  Crear usuario
                 </button>
               </>
             ) : (
